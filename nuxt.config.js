905aedca09cb74185d3d57c0a711a1894d0c4712
fzl <fzl@douyait.com>
2020-04-12 02:37:49 +0800

    修改 easy-mock 地址

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,56 +1,60 @@
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
-    meta: [{
-        charset: 'utf-8'
+    title: process.env.npm_package_name || "",
+    meta: [
+      {
+        charset: "utf-8"
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
       },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
       }
     ],
-    link: [{
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon.ico'
-    }]
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "/favicon.ico"
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
    */
   loading: {
-    color: '#55cbc4'
+    color: "#55cbc4"
   },
   /*
    ** Global CSS
    */
   css: [
-    'element-ui/lib/theme-chalk/index.css',
-    '~/assets/css/reset.css',
-    '~/assets/css/animate.css',
-    '~/assets/css/transition.css',
-    '~/assets/css/iconfont.css',
+    "element-ui/lib/theme-chalk/index.css",
+    "~/assets/css/reset.css",
+    "~/assets/css/animate.css",
+    "~/assets/css/transition.css",
+    "~/assets/css/iconfont.css"
   ],
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [{
-      src: '@/plugins/element-ui',
+  plugins: [
+    {
+      src: "@/plugins/element-ui",
       ssr: false
     },
     {
-      src: '@/plugins/router',
+      src: "@/plugins/router",
       ssr: false
-    },
+    }
   ],
   /*
    ** Nuxt.js dev-modules
@@ -59,12 +63,9 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/style-resources'
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/style-resources"],
   styleResources: {
-    scss: './assets/css/common.scss'
+    scss: "./assets/css/common.scss"
   },
   /*
    ** Build configuration
@@ -74,17 +75,20 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {},
+    extend(config, ctx) {}
   },
   //全局变量
-  env: process.env.NODE_ENV === 'development' ? {
-    BASE_URL: 'https://www.studyinghome.com/mock/5e5618d82cb0d073b813a629/boss'
-  } : {
-    BASE_URL: 'https://www.studyinghome.com/mock/5e5618d82cb0d073b813a629/boss'
-  },
+  env:
+    process.env.NODE_ENV === "development"
+      ? {
+          BASE_URL: "http://mock.alcyh.com/mock/5e840218ab3f3634c357d8bf/boss"
+        }
+      : {
+          BASE_URL: "http://mock.alcyh.com/mock/5e840218ab3f3634c357d8bf/boss"
+        },
   //路由
   router: {
-    base: '',
+    base: ""
     // middleware: '',
   }
-}
+};
